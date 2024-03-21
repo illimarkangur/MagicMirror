634ec26616ba1e47d54db4f9515406ce6f80a364
Haavi <javier@vhk.ee>
2024-03-21 09:17:20 +0200

    Small fix in for loop

--- modules/default/paevajaanud/paevajaanud.js
+++ modules/default/paevajaanud/paevajaanud.js
@@ -1,58 +1,57 @@
 Module.register("paevajaanud", {
+  // vaikimisi config
+  defaults: {
+    textColor: "white",
+    dates: [
+      // sisesta kuupäev formaadis 'dd/MM/yyyy'
+      { name: "Aasta 2025", date: "01/01/2025" },
+      { name: "Aasta 2026", date: "01/01/2026" },
+    ],
+  },
 
-    // vaikimisi config
-    defaults: {
-        textColor: "white",
-        dates:[
-          // sisesta kuupäev formaadis 'dd/MM/yyyy'
-            {name:"Aasta 2025", date:"01/01/2025"}, 
-            {name:"Aasta 2026", date:"01/01/2026"}
-        ]
-      },
+  start: function () {
+    var self = this;
+    setInterval(function () {
+      self.updateDom(); // no speed defined, so it updates instantly.
+    }, 1000); //perform every 1000 milliseconds.
+  },
 
-      start: function() {
-        var self = this;
-        setInterval(function() {
-          self.updateDom(); // no speed defined, so it updates instantly.
-        }, 1000); //perform every 1000 milliseconds.
-      },
-  
-      getDaysLeft: function (i) {
-          const date = Date.now(); 
+  getDaysLeft: function (i) {
+    const date = Date.now();
 
-          var formattedDate = dateFormat(this.config.dates[i].date)          
-          const targetDate = new Date(formattedDate);
+    var formattedDate = dateFormat(this.config.dates[i].date);
+    const targetDate = new Date(formattedDate);
 
-          const difference = targetDate - date; 
+    const difference = targetDate - date;
 
-          // tagastab päevade arvu, jagades ajavahet millisekundite arvuga ühes päevas  
-          return Math.floor(difference/(1000 * 60 * 60 * 24)) 
-      },
-  
-      //võtab configis kirjutatud dd/MM/yyyy formati ja tõstab selle ümber MM/dd/yyyy formaati, mis on Date() funktsioonis vajalik
-      dateFormat: function (date) {
-  
-        const dd = date.split('/')[0]; 
-        const mm = date.split('/')[1]; 
-        const yyyy = date.split('/')[2];
-  
-        const formattedDate = mm + '/' + dd + '/' + yyyy;
-        return formattedDate
-      },
+    // tagastab päevade arvu, jagades ajavahet millisekundite arvuga ühes päevas
+    return Math.floor(difference / (1000 * 60 * 60 * 24));
+  },
 
-    // mooduli elemendid ja stiil
-    getDom: function () {
-      var wrapper = document.createElement("div");
-      wrapper.style.color = this.config.textColor;
+  //võtab configis kirjutatud dd/MM/yyyy formati ja tõstab selle ümber MM/dd/yyyy formaati, mis on Date() funktsioonis vajalik
+  dateFormat: function (date) {
+    const dd = date.split("/")[0];
+    const mm = date.split("/")[1];
+    const yyyy = date.split("/")[2];
 
-      for (let i=0; i < this.config.dates.length; i++) {
-        var text = document.createTextNode(this.config.dates[i].name + ": " + getDaysLeft(i) + " päeva.");
+    const formattedDate = mm + "/" + dd + "/" + yyyy;
+    return formattedDate;
+  },
 
-        wrapper.appendChild(text);
-        
-        var br = document.createElement("br");
-        wrapper.appendChild(br)
-      }
-      return wrapper;
+  // mooduli elemendid ja stiil
+  getDom: function () {
+    var wrapper = document.createElement("div");
+    wrapper.style.color = this.config.textColor;
+
+    for (let i in this.config.dates) {
+      var text = document.createTextNode(
+        this.config.dates[i].name + ": " + getDaysLeft(i) + " päeva."
+      );
+
+      wrapper.appendChild(text);
+
+      wrapper.appendChild(document.createElement("br"));
     }
-  });
+    return wrapper;
+  },
+});
